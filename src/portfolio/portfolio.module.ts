29ad28f3cf6a7ad83a946df4b7f042469c461576
raizo07 <agadaraizo@gmail.com>
2026-01-30 00:59:14 +0100

    update the portfolio module and service

--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -4,17 +4,20 @@ import { PortfolioService } from './portfolio.service';
 import { PortfolioController } from './portfolio.controller';
 import { Trade } from '../trades/entities/trade.entity';
 import { Position } from './entities/position.entity';
+import { PnlHistory } from './entities/pnl-history.entity';
 import { User } from '../users/entities/user.entity';
 import { PriceService } from '../shared/price.service';
 import { CacheModule } from '@nestjs/cache-manager';
+import { PnlCalculatorService } from './services/pnl-calculator.service';
+import { PerformanceTrackerService } from './services/performance-tracker.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Trade, Position, User]),
+    TypeOrmModule.forFeature([Trade, Position, User, PnlHistory]),
     CacheModule.register(),
   ],
   controllers: [PortfolioController],
-  providers: [PortfolioService, PriceService],
-  exports: [PortfolioService],
+  providers: [PortfolioService, PriceService, PnlCalculatorService, PerformanceTrackerService],
+  exports: [PortfolioService, PnlCalculatorService, PerformanceTrackerService],
 })
 export class PortfolioModule {}
